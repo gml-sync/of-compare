ca46766d33f28cdd1548b8e41a216f8039589b5e
bot <example@gmail.com>
2021-04-28 12:51:09 +0300

    input frames

---
+++ run.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def subp_run_str(cmd, output=True):
+    print('RUN:', cmd)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+
+    if output:
+        for line in process.stdout:
+            print(line.decode(), end='')
+    rc = process.poll()
+    return rc
+
+def subp_bash(cmd):
+    subp_run_str("bash -c '" + cmd + "'")
+
+
+
+
+# Split frames
+def frame_ffmpeg_split(video_path, output_folder):
+    subp_run_str(['ffmpeg -i ' + video_path +
+            ' -qscale:v 2 ' + output_folder + '/frame_%04d.jpg'])
+
