6aae315b5b64d5b48fae4d08863a95b6054c4621
bot <example@gmail.com>
2021-04-28 12:56:35 +0300

    1

--- run.py
+++ run.py
@@ -21,3 +21,6 @@ def frame_ffmpeg_split(video_path, output_folder):
     subp_run_str(['ffmpeg -i ' + video_path +
             ' -qscale:v 2 ' + output_folder + '/frame_%04d.jpg'])
 
+
+os.makedirs('frames')
+frame_ffmpeg_split('vids/09_l0.mkv', 'frames')
